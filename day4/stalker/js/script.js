fb501099864b6ec823c3e5e7136c24962c19d935
Neerajmaddimsetty <66665765+Neerajmaddimsetty@users.noreply.github.com>
2020-06-09 13:38:45 +0530

    Add files via upload

---
+++ day4/stalker/js/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your name?");
+var lastName = prompt("What is your lastName");
+var age = prompt("What is your age");
+
+var fullName = firstName + " " +lastName
+
+console.log("Your full name is"+fullName);
+console.log("You are" + age + "years old");
